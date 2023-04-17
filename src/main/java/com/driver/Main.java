60bec621fac3654c868184f49ffb440f5e670377
Neha Ahire <neha.dani2011@gmail.com>
2023-04-18 00:03:26 +0530

    Encaplusation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args){
+      RWOnly r1 = new RWOnly();
+
+      //r1.name("John")  this will cause compilation error because name is private variable
+     // System.out.println(r1.name());
+
+      r1.setName("John");
+      System.out.println(r1.getName());
+
+
+  }
 }
